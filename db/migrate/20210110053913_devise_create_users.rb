01152e04a689195300e4f541e5a75b70fc9a8204
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-01-18 23:31:38 +0900

    デバイスを削除しジェムの再インストール

--- db/migrate/20210110053913_devise_create_users.rb
+++ db/migrate/20210110053913_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false, default: ""
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.date   :birthday,           null: false, 
-      t.string :kana_first_name
-      t.string :kana_family_name
-      t.string :kanji_first_name
-      t.string :kanji_family_name
+      t.date   :birthday,           null: false
+      t.string :kana_first_name,    null: false
+      t.string :kana_family_name,   null: false
+      t.string :kanji_first_name,   null: false
+      t.string :kanji_family_name,  null: false
 
       ## Recoverable
       t.string   :reset_password_token
