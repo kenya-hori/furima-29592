d3dfc6ffc1317f7db85dd3cb0a4e54e1d15279f2
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-02-07 20:28:34 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  with_options presence: true do
+    validates :nickname, :birthday
+    validates :kanji_family_name, :kanji_first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :kana_family_name, :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  end
 end
