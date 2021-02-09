671acb3ea4765a81fac0e06ceaa25987cfcf502a
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-02-09 20:01:16 +0900

    テストコードの下準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    kana_first_name       {'ゲッコウ'}
+    kana_family_name      {'ラン'}
+    kanji_first_name      {'激昂'}
+    kanji_family_name     {'蘭'}
+    birthday              {'1981-01-27'}
+  end
+end
