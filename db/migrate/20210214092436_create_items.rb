d1ab2971760ee075ab62329e6fae61022bff7431
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-02-14 18:57:13 +0900

    マイグレーションの設定

--- db/migrate/20210214092436_create_items.rb
+++ db/migrate/20210214092436_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.references    :user_id                ,null: false, foreign_key: true
+      t.string        :name                   ,null: false
+      t.text          :text                   ,null: false
+      t.integer       :price                  ,null: false
+      t.integer       :category_id            ,null: false
+      t.integer       :state_id               ,null: false
+      t.integer       :delivery_fee_burden_id ,null: false
+      t.integer       :prefecture_id          ,null: false
+      t.integer       :day_to_ship_id         ,null: false
       t.timestamps
     end
   end
