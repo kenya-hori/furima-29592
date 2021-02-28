eb487ff7f855714227158bd0a01dfad956a5338b
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-02-28 18:45:08 +0900

    テスト項目の精査

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の新規登録' do
+    context '新規登録できる時' do
+      it 'image、name、text、price、が存在し、category_id、condition_id、delivery_fee_burden_id、prefecture_id、day_to_ship_idが１以外であれば登録できる' do
+        
+      end
+      it 'priceが300から9999999の間であれば登録できる' do
+        
+      end
+    end
+
+    context '新規登録できない時' do
+      it 'imageが空だと登録できない' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+      it '' do
+        
+      end
+    end
+  end
 end
