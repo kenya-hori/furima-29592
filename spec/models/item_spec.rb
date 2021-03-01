66047790f807eac8f829c568781ce2254cbdc250
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-03-01 21:22:13 +0900

    指摘修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it 'priceが300から9999999の間であれば登録できる' do
-        @item.price = '12345'
+        @item.price = 12345
         expect(@item).to be_valid
       end
     end
@@ -37,13 +37,13 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price can't be blank")
       end
-      it 'priceが299以下だとと登録できない' do
-        @item.price = '200'
+      it 'priceが299以下だと登録できない' do
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price Out of setting range")
       end
       it 'priceが10000000以上だと登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price Out of setting range")
       end
@@ -63,27 +63,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include ("Price Half-width number")
       end
       it 'category_idが1だと登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include ("Category Sellect")
       end
       it 'condition_idが1だと登録できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include ("Condition Sellect")
       end
       it 'delivery_fee_burden_idが1だと登録できない' do
-        @item.delivery_fee_burden_id = '1'
+        @item.delivery_fee_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include ("Delivery fee burden Sellect")
       end
       it 'prefecture_idが1だと登録できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include ("Prefecture Sellect")
       end
       it 'day_to_ship_idが1だと登録できない' do
-        @item.day_to_ship_id = '1'
+        @item.day_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include ("Day to ship Sellect")
       end
