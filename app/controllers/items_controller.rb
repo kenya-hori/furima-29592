6b31066826e321cb4204e94825a4cbb2428f4fb2
kenya-hori <72641777+kenya-hori@users.noreply.github.com>
2021-03-12 16:56:03 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,6 +45,8 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to user_session_path
+    end
   end
 end
